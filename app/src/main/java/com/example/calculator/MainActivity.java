d4a69523c12e57f3a92c2c95447859f9b5a3f006
woo-ara <war0426@naver.com>
2021-09-19 16:05:21 +0900

    간단한 계산기

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void onClickAdd(View view) {
+        EditText number1 = (EditText) findViewById(R. id.editNum1);
+        EditText number2 = (EditText) findViewById(R. id.editNum2);
+        TextView result = (TextView) findViewById(R. id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+
+        result.setText(Integer.toString(n1 + n2));
+    }
+
+    public void onClickSub(View view) {
+        EditText number1 = (EditText) findViewById(R. id.editNum1);
+        EditText number2 = (EditText) findViewById(R. id.editNum2);
+        TextView result = (TextView) findViewById(R. id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+
+        result.setText(Integer.toString(n1 - n2));
+    }
+
+    public void onClickMul(View view) {
+        EditText number1 = (EditText) findViewById(R. id.editNum1);
+        EditText number2 = (EditText) findViewById(R. id.editNum2);
+        TextView result = (TextView) findViewById(R. id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+
+        result.setText(Integer.toString(n1 * n2));
+    }
+
+    public void onClickDiv(View view) {
+        EditText number1 = (EditText) findViewById(R. id.editNum1);
+        EditText number2 = (EditText) findViewById(R. id.editNum2);
+        TextView result = (TextView) findViewById(R. id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+
+        result.setText(Integer.toString(n1 / n2));
+    }
+
+}
