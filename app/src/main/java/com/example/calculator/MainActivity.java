6a0dc1cda558dccbe70077a31c9b3d846db75c50
woo-ara <war0426@naver.com>
2021-09-23 21:46:45 +0900

    소수점 되는 간단한 계산기 레이아웃

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -19,61 +19,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
+    // + 버튼 (덧셈)
     public void onClickAdd(View view) {
         EditText number1 = (EditText) findViewById(R. id.editNum1);
         EditText number2 = (EditText) findViewById(R. id.editNum2);
         TextView result = (TextView) findViewById(R. id.result);
 
-//        int n1 = Integer.parseInt(number1.getText().toString());
-//        int n2 = Integer.parseInt(number2.getText().toString());
-//
-//        result.setText(Integer.toString(n1 + n2));
         double n1 = Double.parseDouble(number1.getText().toString());
         double n2 = Double.parseDouble(number2.getText().toString());
 
         result.setText(Double.toString(n1 + n2));
     }
 
+    // - 버튼 (뺄셈)
     public void onClickSub(View view) {
         EditText number1 = (EditText) findViewById(R. id.editNum1);
         EditText number2 = (EditText) findViewById(R. id.editNum2);
         TextView result = (TextView) findViewById(R. id.result);
 
-//        int n1 = Integer.parseInt(number1.getText().toString());
-//        int n2 = Integer.parseInt(number2.getText().toString());
-//
-//        result.setText(Integer.toString(n1 - n2));
         double n1 = Double.parseDouble(number1.getText().toString());
         double n2 = Double.parseDouble(number2.getText().toString());
 
         result.setText(Double.toString(n1 - n2));
     }
 
+    // * 버튼 (곱셈)
     public void onClickMul(View view) {
         EditText number1 = (EditText) findViewById(R. id.editNum1);
         EditText number2 = (EditText) findViewById(R. id.editNum2);
         TextView result = (TextView) findViewById(R. id.result);
 
-//        int n1 = Integer.parseInt(number1.getText().toString());
-//        int n2 = Integer.parseInt(number2.getText().toString());
-//
-//        result.setText(Integer.toString(n1 * n2));
         double n1 = Double.parseDouble(number1.getText().toString());
         double n2 = Double.parseDouble(number2.getText().toString());
 
         result.setText(Double.toString(n1 * n2));
     }
 
+    // / 버튼 (나눗셈)
     public void onClickDiv(View view) {
         EditText number1 = (EditText) findViewById(R. id.editNum1);
         EditText number2 = (EditText) findViewById(R. id.editNum2);
         TextView result = (TextView) findViewById(R. id.result);
 
-//        int n1 = Integer.parseInt(number1.getText().toString());
-//        int n2 = Integer.parseInt(number2.getText().toString());
-//
-//        result.setText(Integer.toString(n1 / n2));
         double n1 = Double.parseDouble(number1.getText().toString());
         double n2 = Double.parseDouble(number2.getText().toString());
 
